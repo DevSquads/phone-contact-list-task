555907e18107d381ef26de88738c6b9588ae65e1
asmaa <asmaaatahaa@gmail.com>
2019-06-27 21:15:02 +0200

    Add Default Data

---
+++ app1/data.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        Name: 'Ahmed',
+        JobTitle: 'title1',
+        Image: ''
+    },
+    {
+        Name: 'Sara',
+        JobTitle: 'title2',
+        Image: ''
+    },
+    {
+        Name: 'Hadeer',
+        JobTitle: 'title3',
+        Image: ''
+    },
+    {
+        Name: 'Alyaa',
+        JobTitle: 'title4',
+        Image: ''
+    },
+    {
+        Name: 'Doaa',
+        JobTitle: 'title5',
+        Image: ''
+    },
+    {
+        Name: 'Samar',
+        JobTitle: 'title6',
+        Image: ''
+    },
+    {
+        Name: 'Soha',
+        JobTitle: 'title7',
+        Image: ''
+    },
+    {
+        Name: 'Nehal',
+        JobTitle: 'title8',
+        Image: ''
+    },
+    {
+        Name: 'Mohamed',
+        JobTitle: 'title9',
+        Image: ''
+    },
+    {
+        Name: 'Ali',
+        JobTitle: 'title10',
+        Image: ''
+    },
+];
