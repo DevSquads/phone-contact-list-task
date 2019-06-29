8b0f241d98e98d00fa59d2aa3838c5f4a7fa74fa
Fatma Reda <fatmar883@gmail.com>
2019-06-30 00:49:45 +0200

    add proptypes and defaultprops in ContactCard, add search functionality in ContactList

--- src/Components/ContactCard/ContactCard.js
+++ src/Components/ContactCard/ContactCard.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
+import Proptype from "prop-types";
+import Img from "../../assets/defaultImg.jpg";
 import "./ContactCard.module.css";
 
 const ContactCard = props => {
   return (
     <React.Fragment>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h2>{props.contactName}</h2>
-          </div>
-        </div>
-      </div>
+      <h2>{props.contactName}</h2>
+      <p>{props.contactInfo}</p>
     </React.Fragment>
   );
 };
 
+ContactCard.proptype = {
+  contactName: Proptype.string.isRequired,
+  imgURL: Proptype.string,
+  contactInfo: Proptype.string
+};
+
+ContactCard.defaultProps = {
+  imgURL: Img,
+  contactInfo: "web Developer"
+};
 export default ContactCard;
--- src/Containers/ContactList/ContactList.js
+++ src/Containers/ContactList/ContactList.js
@@ -11,8 +11,7 @@ class ContactList extends Component {
         {
           _id: "12345a",
           img: Img,
-          contactName: "Fatma Reda",
-          contactInfo: "Web Developer"
+          contactName: "Fatma Reda"
         },
         {
           _id: "12345b",
@@ -25,14 +24,32 @@ class ContactList extends Component {
           img: Img,
           contactName: "Hager Hassan",
           contactInfo: "Marketer"
+        },
+        {
+          _id: "12345d",
+          img: Img,
+          contactName: "Fatma Alaa",
+          contactInfo: "Marketer"
+        },
+        {
+          _id: "12345e",
+          contactName: "Ahmed Waheed"
         }
-      ]
+      ],
+      search: ""
     };
   }
-
+  changeHandler = event => {
+    this.setState({
+      search: event.target.value.substr(0, 15)
+    });
+  };
   render() {
-    let contactList = this.state.contacts ? (
-      this.state.contacts.map(contact => {
+    let filteredContacts = this.state.contacts.filter((c)=>{
+      return c.contactName.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+    });
+    let contactList = filteredContacts ? (
+      filteredContacts.map(contact => {
         return (
           <li className="list-group-item" key={contact._id}>
             <ContactCard
@@ -47,9 +64,19 @@ class ContactList extends Component {
       <div>not found</div>
     );
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 d-flex justify-content-center">
+      <div className="container ContactList">
+        <div className="row d-flex justify-content-center">
+          <div className="col-md-6 ContactList-head">
+            <h4 className="ContactList-head-title"> Contact List</h4><br/>
+            <input
+              value={this.state.search}
+              onChange={event => this.changeHandler(event)}
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-md-8">
             <ul className="list-group list-group-flush">{contactList}</ul>
           </div>
         </div>
