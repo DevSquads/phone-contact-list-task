853958723a0eb1deb2dcfd89796d862ad7b8cfab
Muslim <doaaismael@outlook.com>
2019-06-30 23:17:57 +0200

    add contact fake service and dummy data

---
+++ phone-contacts/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ContactList from './components/contact-list.jsx';
+
+function App() {
+  return (
+    <ContactList />
+  );
+}
+
+export default App;
