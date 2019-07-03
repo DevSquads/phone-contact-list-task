36f6294f92aef60b4486c1d9b6269d1b85f434cf
aml <aml.fci@gmail.com>
2019-07-03 03:23:58 +0200

    testing search component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import contactList from './contactList';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App component', () => {
+  it('starts with a count of 0', () => {
+    const cas = shallow(<App />);
+    expect(cas.find(contactList)).toHaveLengthOf(1);
+  });
 });
