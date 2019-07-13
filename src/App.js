bea45139e2e7415fc4a69675a8ba27b387678aba
Ahmed Sami <samidevmail@gmail.com>
2019-07-13 15:04:57 +0200

    Added Header and Search Area

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// Libraries Imports
+import React from "react";
+
+// Screens Imports
+import ContactsList from "./Screens/ContactsList";
+
+export default class App extends React.Component {
+  render() {
+    return <ContactsList />;
+  }
+}
---
+++ src/Screens/ContactsList.js
@@ -0,0 +1,139 @@
+// Libraries Imports
+import React from "react";
+
+// Components Imoprts
+import {
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  TextInput,
+  Keyboard,
+  Platform
+} from "react-native";
+import Icon from "react-native-vector-icons/EvilIcons";
+import * as Animatable from "react-native-animatable";
+
+export default class ContactsList extends React.Component {
+  state = {
+    search: "",
+    searchBarFocused: false
+  };
+
+  // Implementing life-cycle Functions
+  componentDidMount() {
+    if (Platform.OS === "ios") {
+      this.keyboardWillShow = Keyboard.addListener(
+        "keyboardWillShow",
+        this.keyboardWillShow
+      );
+    } else {
+      this.keyboardDidShow = Keyboard.addListener(
+        "keyboardDidShow",
+        this.keyboardDidShow
+      );
+    }
+    this.keyboardWillHide = Keyboard.addListener(
+      "keyboardWillHide",
+      this.keyboardWillHide
+    );
+  }
+
+  render() {
+    const { search } = this.state;
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar backgroundColor="#00529B" />
+        <View style={styles.header}>
+          <Text style={styles.name}>CONTACTS LIST</Text>
+          <Animatable.View
+            style={styles.inputContainer}
+            animation="slideInRight"
+            duration={500}
+          >
+            <Animatable.View
+              animation={
+                this.state.searchBarFocused ? "fadeInLeft" : "fadeInRight"
+              }
+            >
+              <Icon
+                name={this.state.searchBarFocused ? "chevron-left" : "search"}
+                color="#fff"
+                size={30}
+                onPress={() => this.inputBlur()}
+              />
+            </Animatable.View>
+            <TextInput
+              placeholder="Search Contacts ..."
+              style={styles.input}
+              placeholderTextColor="#fff"
+              value={search}
+              onChangeText={this.updateSearch}
+              ref={ref => (this.ref = ref)}
+            />
+          </Animatable.View>
+        </View>
+      </View>
+    );
+  }
+
+  // -------- Event Handlers ---------
+
+  // Input Event Handlers
+  updateSearch = search => {
+    this.setState({ search });
+  };
+  inputBlur() {
+    this.ref.blur();
+    this.setState({ searchBarFocused: false });
+  }
+
+  // Keyboard Event Handlers
+  keyboardDidShow = () => {
+    this.setState({ searchBarFocused: true });
+  };
+  keyboardWillShow = () => {
+    this.setState({ searchBarFocused: true });
+  };
+  keyboardWillHide = () => {
+    this.setState({ searchBarFocused: false });
+  };
+}
+
+// Styles
+const styles = StyleSheet.create({
+  header: {
+    height: 120,
+    backgroundColor: "#00529B",
+    shadowColor: "#5A685B",
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    elevation: 2,
+    alignItems: "center"
+  },
+  name: {
+    fontSize: 20,
+    fontFamily: "Roboto-Medium",
+    color: "#fff",
+    paddingTop: 20
+  },
+  inputContainer: {
+    flexDirection: "row",
+    padding: 5,
+    alignItems: "center",
+    height: 40,
+    backgroundColor: "#005bac",
+    borderRadius: 100,
+    width: "75%",
+    marginTop: 20
+  },
+  input: {
+    height: 40,
+    flex: 1,
+    color: "#fff"
+  }
+});
